f55926e54263fcf8b8b63297bdf66347b29a190c
Volodymyr <volodymyr.sekh.dev@gmail.com>
2022-04-01 12:06:43 +0300

    add menu

---
+++ src/Menu.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+export default  function Menu(){
+  return (
+<nav className ="Menu">
+  
+<ul>
+  <li>
+  <a className="navbar-brand" href="#home">
+      
+            <img src="./logo192.png" width="24" alt="logo" height="24"></img>
+      
+</a>
+  </li>
+  <div className ="navBar">
+  <li><a  href="#home">Home </a></li>
+  <li><a href="#catalog">Catalog </a></li>
+  <li><a href="#cart">Cart </a></li>
+  </div>
+</ul>
+</nav>
+  )
+}
